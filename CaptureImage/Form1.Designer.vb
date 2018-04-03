0e685a0f9f2a8d1430063a10c53f04bd8536f99c
Antony Ingram <vrosnet@gmail.com>
2018-04-04 05:29:46 +1000

    Resistor colour codes
    Resistor colour code calculator with video scanner to check resistors. You can use a USB microscope and it will get the colour codes and convert to the resistance value. Not complete yet.

---
+++ CaptureImage/Form1.Designer.vb
@@ -0,0 +1,398 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.txtValueX = New System.Windows.Forms.TextBox()
+        Me.txtValueY = New System.Windows.Forms.TextBox()
+        Me.txtValueColor = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.txtValueX2 = New System.Windows.Forms.TextBox()
+        Me.txtValueY2 = New System.Windows.Forms.TextBox()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.Panel6 = New System.Windows.Forms.Panel()
+        Me.Panel7 = New System.Windows.Forms.Panel()
+        Me.Panel8 = New System.Windows.Forms.Panel()
+        Me.Panel9 = New System.Windows.Forms.Panel()
+        Me.Panel10 = New System.Windows.Forms.Panel()
+        Me.Panel11 = New System.Windows.Forms.Panel()
+        Me.Panel12 = New System.Windows.Forms.Panel()
+        Me.Panel13 = New System.Windows.Forms.Panel()
+        Me.Panel14 = New System.Windows.Forms.Panel()
+        Me.Panel15 = New System.Windows.Forms.Panel()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.lblX = New System.Windows.Forms.Label()
+        Me.lX = New System.Windows.Forms.Label()
+        Me.lxlY = New System.Windows.Forms.Label()
+        Me.lY = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(321, 258)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button1.Location = New System.Drawing.Point(9, 329)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 22)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button2.Location = New System.Drawing.Point(86, 329)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 22)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Capture"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(167, 329)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "ClearImg"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'txtValueX
+        '
+        Me.txtValueX.Location = New System.Drawing.Point(9, 354)
+        Me.txtValueX.Name = "txtValueX"
+        Me.txtValueX.Size = New System.Drawing.Size(31, 20)
+        Me.txtValueX.TabIndex = 6
+        '
+        'txtValueY
+        '
+        Me.txtValueY.Location = New System.Drawing.Point(43, 354)
+        Me.txtValueY.Name = "txtValueY"
+        Me.txtValueY.Size = New System.Drawing.Size(31, 20)
+        Me.txtValueY.TabIndex = 7
+        '
+        'txtValueColor
+        '
+        Me.txtValueColor.Location = New System.Drawing.Point(78, 354)
+        Me.txtValueColor.Name = "txtValueColor"
+        Me.txtValueColor.Size = New System.Drawing.Size(190, 20)
+        Me.txtValueColor.TabIndex = 8
+        '
+        'Panel1
+        '
+        Me.Panel1.Location = New System.Drawing.Point(78, 377)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(53, 20)
+        Me.Panel1.TabIndex = 9
+        '
+        'txtValueX2
+        '
+        Me.txtValueX2.Location = New System.Drawing.Point(9, 377)
+        Me.txtValueX2.Name = "txtValueX2"
+        Me.txtValueX2.Size = New System.Drawing.Size(31, 20)
+        Me.txtValueX2.TabIndex = 12
+        '
+        'txtValueY2
+        '
+        Me.txtValueY2.Location = New System.Drawing.Point(43, 377)
+        Me.txtValueY2.Name = "txtValueY2"
+        Me.txtValueY2.Size = New System.Drawing.Size(31, 20)
+        Me.txtValueY2.TabIndex = 13
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.Black
+        Me.Panel2.Location = New System.Drawing.Point(352, 12)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(53, 14)
+        Me.Panel2.TabIndex = 16
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.Brown
+        Me.Panel3.Location = New System.Drawing.Point(352, 30)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(53, 14)
+        Me.Panel3.TabIndex = 17
+        '
+        'Panel4
+        '
+        Me.Panel4.BackColor = System.Drawing.Color.Red
+        Me.Panel4.Location = New System.Drawing.Point(352, 48)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(53, 14)
+        Me.Panel4.TabIndex = 18
+        '
+        'Panel5
+        '
+        Me.Panel5.BackColor = System.Drawing.Color.Orange
+        Me.Panel5.Location = New System.Drawing.Point(352, 66)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(53, 14)
+        Me.Panel5.TabIndex = 19
+        '
+        'Panel6
+        '
+        Me.Panel6.BackColor = System.Drawing.Color.Yellow
+        Me.Panel6.Location = New System.Drawing.Point(352, 84)
+        Me.Panel6.Name = "Panel6"
+        Me.Panel6.Size = New System.Drawing.Size(53, 14)
+        Me.Panel6.TabIndex = 20
+        '
+        'Panel7
+        '
+        Me.Panel7.BackColor = System.Drawing.Color.Green
+        Me.Panel7.Location = New System.Drawing.Point(352, 102)
+        Me.Panel7.Name = "Panel7"
+        Me.Panel7.Size = New System.Drawing.Size(53, 14)
+        Me.Panel7.TabIndex = 21
+        '
+        'Panel8
+        '
+        Me.Panel8.BackColor = System.Drawing.Color.Blue
+        Me.Panel8.Location = New System.Drawing.Point(352, 120)
+        Me.Panel8.Name = "Panel8"
+        Me.Panel8.Size = New System.Drawing.Size(53, 14)
+        Me.Panel8.TabIndex = 22
+        '
+        'Panel9
+        '
+        Me.Panel9.BackColor = System.Drawing.Color.Violet
+        Me.Panel9.Location = New System.Drawing.Point(352, 138)
+        Me.Panel9.Name = "Panel9"
+        Me.Panel9.Size = New System.Drawing.Size(53, 14)
+        Me.Panel9.TabIndex = 23
+        '
+        'Panel10
+        '
+        Me.Panel10.BackColor = System.Drawing.Color.Gray
+        Me.Panel10.Location = New System.Drawing.Point(352, 156)
+        Me.Panel10.Name = "Panel10"
+        Me.Panel10.Size = New System.Drawing.Size(53, 14)
+        Me.Panel10.TabIndex = 24
+        '
+        'Panel11
+        '
+        Me.Panel11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel11.Location = New System.Drawing.Point(352, 174)
+        Me.Panel11.Name = "Panel11"
+        Me.Panel11.Size = New System.Drawing.Size(53, 14)
+        Me.Panel11.TabIndex = 25
+        '
+        'Panel12
+        '
+        Me.Panel12.BackColor = System.Drawing.Color.Gold
+        Me.Panel12.Location = New System.Drawing.Point(352, 205)
+        Me.Panel12.Name = "Panel12"
+        Me.Panel12.Size = New System.Drawing.Size(53, 14)
+        Me.Panel12.TabIndex = 26
+        '
+        'Panel13
+        '
+        Me.Panel13.BackColor = System.Drawing.Color.Silver
+        Me.Panel13.Location = New System.Drawing.Point(352, 223)
+        Me.Panel13.Name = "Panel13"
+        Me.Panel13.Size = New System.Drawing.Size(53, 14)
+        Me.Panel13.TabIndex = 27
+        '
+        'Panel14
+        '
+        Me.Panel14.BackColor = System.Drawing.Color.Transparent
+        Me.Panel14.Location = New System.Drawing.Point(352, 241)
+        Me.Panel14.Name = "Panel14"
+        Me.Panel14.Size = New System.Drawing.Size(53, 14)
+        Me.Panel14.TabIndex = 28
+        '
+        'Panel15
+        '
+        Me.Panel15.Location = New System.Drawing.Point(137, 377)
+        Me.Panel15.Name = "Panel15"
+        Me.Panel15.Size = New System.Drawing.Size(53, 20)
+        Me.Panel15.TabIndex = 29
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"3 band", "4 band", "5 band", "6 band"})
+        Me.ComboBox1.Location = New System.Drawing.Point(9, 302)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(78, 21)
+        Me.ComboBox1.TabIndex = 30
+        Me.ComboBox1.Text = "5 band"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(93, 302)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 31
+        Me.Button3.Text = "Draw Bands"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(167, 302)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 32
+        Me.Button5.Text = "MakeLines"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'lblX
+        '
+        Me.lblX.AutoSize = True
+        Me.lblX.Location = New System.Drawing.Point(261, 310)
+        Me.lblX.Name = "lblX"
+        Me.lblX.Size = New System.Drawing.Size(17, 13)
+        Me.lblX.TabIndex = 34
+        Me.lblX.Text = "X:"
+        '
+        'lX
+        '
+        Me.lX.AutoSize = True
+        Me.lX.Location = New System.Drawing.Point(272, 310)
+        Me.lX.Name = "lX"
+        Me.lX.Size = New System.Drawing.Size(13, 13)
+        Me.lX.TabIndex = 35
+        Me.lX.Text = "0"
+        '
+        'lxlY
+        '
+        Me.lxlY.AutoSize = True
+        Me.lxlY.Location = New System.Drawing.Point(316, 310)
+        Me.lxlY.Name = "lxlY"
+        Me.lxlY.Size = New System.Drawing.Size(17, 13)
+        Me.lxlY.TabIndex = 37
+        Me.lxlY.Text = "Y:"
+        '
+        'lY
+        '
+        Me.lY.AutoSize = True
+        Me.lY.Location = New System.Drawing.Point(329, 310)
+        Me.lY.Name = "lY"
+        Me.lY.Size = New System.Drawing.Size(13, 13)
+        Me.lY.TabIndex = 38
+        Me.lY.Text = "0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(418, 399)
+        Me.Controls.Add(Me.lY)
+        Me.Controls.Add(Me.lxlY)
+        Me.Controls.Add(Me.lX)
+        Me.Controls.Add(Me.lblX)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Panel15)
+        Me.Controls.Add(Me.Panel14)
+        Me.Controls.Add(Me.Panel13)
+        Me.Controls.Add(Me.Panel12)
+        Me.Controls.Add(Me.Panel11)
+        Me.Controls.Add(Me.Panel10)
+        Me.Controls.Add(Me.Panel9)
+        Me.Controls.Add(Me.Panel8)
+        Me.Controls.Add(Me.Panel7)
+        Me.Controls.Add(Me.Panel6)
+        Me.Controls.Add(Me.Panel5)
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.txtValueY2)
+        Me.Controls.Add(Me.txtValueX2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.txtValueColor)
+        Me.Controls.Add(Me.txtValueY)
+        Me.Controls.Add(Me.txtValueX)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Capture Resistor Image Decode Values"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Button4 As Button
+    Friend WithEvents txtValueX As TextBox
+    Friend WithEvents txtValueY As TextBox
+    Friend WithEvents txtValueColor As TextBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents txtValueX2 As TextBox
+    Friend WithEvents txtValueY2 As TextBox
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents Panel6 As Panel
+    Friend WithEvents Panel7 As Panel
+    Friend WithEvents Panel8 As Panel
+    Friend WithEvents Panel9 As Panel
+    Friend WithEvents Panel10 As Panel
+    Friend WithEvents Panel11 As Panel
+    Friend WithEvents Panel12 As Panel
+    Friend WithEvents Panel13 As Panel
+    Friend WithEvents Panel14 As Panel
+    Friend WithEvents Panel15 As Panel
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents lblX As Label
+    Friend WithEvents lX As Label
+    Friend WithEvents lxlY As Label
+    Friend WithEvents lY As Label
+End Class
---
+++ CaptureImage/Form1.vb
@@ -0,0 +1,255 @@
+﻿
+Imports Emgu.CV
+Imports Emgu.CV.UI
+Imports Emgu.CV.Structure
+
+Public Class Form1
+
+    '    Protected Overrides Sub OnMouseMove(ByVal mea As MouseEventArgs)
+    '        Cursor.Clip = PictureBox1.Cursor.Clip
+    '        Cursor.Current = Cursors.Cross
+    '    Dim pt As Point 'added by AI
+    '
+    '
+    '    If mea.Button = MouseButtons.Left Then
+    '            pt = New Point(mea.X, mea.Y)
+    '
+    '            lX.Text = Str(pt.X)
+    '            lY.Text = Str(pt.Y)
+    '    End If
+    '    End Sub
+
+
+
+
+
+    Dim cap As New Capture() 'first line
+  
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Timer1.Stop()
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        PictureBox1.Image = cap.QueryFrame.ToBitmap() 'Second line
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Timer1.Start()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        PictureBox1.Image = Nothing
+        'PictureBox1.cls
+
+    End Sub
+
+    Private Sub PictureBox1_MouseDown(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseDown
+        Dim mycolor As Color
+        mycolor = scGetPixel(PictureBox1.Image, e.X, e.Y)
+        lX.Text = Str(e.X)
+        lY.Text = Str(e.Y)
+
+        Panel1.BackColor = mycolor
+        txtValueColor.Text = mycolor.ToString
+        Panel15.BackColor = mQuantize(mycolor)
+    End Sub
+
+    Private Sub PictureBox1_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseMove
+        'Dim mycolor As Color
+        'mycolor = mGetPixel(PictureBox1.Image, e.X, e.Y)
+        'Panel1.BackColor = mycolor
+        'txtValueColor.Text = mycolor.ToString
+        'Panel15.BackColor = mQuantize(mycolor)
+    End Sub
+
+    Private Function scGetPixel(mpb As Image, x As Integer, y As Integer) As Color
+        Dim ScaleFactorX As Single
+        Dim ScaleFactorY As Single
+
+        Dim MyImage As Bitmap = CType(PictureBox1.Image, Bitmap)
+        ScaleFactorX = PictureBox1.Width / MyImage.Width
+        ScaleFactorY = PictureBox1.Height / MyImage.Height
+
+        'Me.BackColor = MyImage.GetPixel(CInt(e.X / ScaleFactorX), CInt(e.Y / ScaleFactorY))
+        Return MyImage.GetPixel(CInt(x / ScaleFactorX), CInt(y / ScaleFactorY))
+    End Function
+
+    Private Function mQuantize(icol As Color) As Color
+        Dim qcol As Color
+
+        Dim r As Integer
+        Dim g As Integer
+        Dim b As Integer
+
+        r = icol.R
+        g = icol.G
+        b = icol.B
+
+        If r < 25 And b < 25 And g < 25 Then qcol = Color.Black
+        If r > 95 And r < 188 And g > 50 And g < 100 And b > 9 And b < 63 Then qcol = Color.Orange
+        Return qcol
+
+    End Function
+
+    Private Sub SetPixel_Example(ByVal e As PaintEventArgs)
+
+        ' Create a Bitmap object from a file.
+        'Dim myBitmap As New Bitmap("Grapes.jpg")
+        Dim myBitmap As New Bitmap(PictureBox1.Image)
+        ' Draw myBitmap to the screen.
+        e.Graphics.DrawImage(myBitmap, 0, 0, myBitmap.Width,
+        myBitmap.Height)
+
+        ' Set each pixel in myBitmap to black.
+        Dim Xcount As Integer
+        For Xcount = 0 To myBitmap.Width - 1
+            Dim Ycount As Integer
+            For Ycount = 0 To myBitmap.Height - 1
+                myBitmap.SetPixel(Xcount, Ycount, Color.Black)
+            Next Ycount
+        Next Xcount
+
+        ' Draw myBitmap to the screen again.
+        e.Graphics.DrawImage(myBitmap, myBitmap.Width, 0, myBitmap.Width,
+            myBitmap.Height)
+    End Sub
+
+    Private Function getMouseLoc() As Point
+        getMouseLoc = Me.PointToClient(Me.Cursor.Position)
+        Return getMouseLoc
+    End Function
+
+    Protected Function TranslateStretchImageMousePosition(ByVal coordinates As Point) As Point
+        ' test to make sure our image is not null
+        If (PictureBox1.Image Is Nothing) Then
+            Return coordinates
+        End If
+
+        ' Make sure our control width and height are not 0
+        If ((Width = 0) _
+                    OrElse (Height = 0)) Then
+            Return coordinates
+        End If
+
+        ' First, get the ratio (image to control) the height and width
+        Dim ratioWidth As Single = (CType(PictureBox1.Image.Width, Single) / Width)
+        Dim ratioHeight As Single = (CType(PictureBox1.Image.Height, Single) / Height)
+        ' Scale the points by our ratio
+        Dim newX As Single = coordinates.X
+        Dim newY As Single = coordinates.Y
+        newX = (newX * ratioWidth)
+        newY = (newY * ratioHeight)
+        Return New Point(CType(newX, Integer), CType(newY, Integer))
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Button3_Click(sender, e)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'MsgBox(ComboBox1.Text)
+        'Dim
+        'MsgBox(ComboBox1.SelectedIndex)
+        DrawBands(ComboBox1.SelectedIndex)
+    End Sub
+
+    Private Sub DrawBands(bands As Integer)
+        'Dim g As Graphics = PictureBox1.CreateGraphics
+        'g.DrawLine(Pens.Black, 100, 0, 100, 100)
+        ''Dim g As Graphics = System.Drawing.Graphics
+        'Dim myPen As New System.Drawing.Pen(System.Drawing.Color.Red)
+        'Dim formGraphics As System.Drawing.Graphics
+        'formGraphics = Me.CreateGraphics()
+        'formGraphics.DrawLine(myPen, 0, 0, 200, 200)
+        'myPen.Dispose()
+        'formGraphics.Dispose()
+        Dim myBrush As New System.Drawing.SolidBrush(System.Drawing.Color.Red)
+        Dim clsBrush As New System.Drawing.SolidBrush(System.Drawing.Color.White)
+        Dim formGraphics As System.Drawing.Graphics
+        formGraphics = Me.CreateGraphics()
+
+        formGraphics.FillRectangle(clsBrush, New Rectangle(12, 0, 330, 40))
+        formGraphics.FillRectangle(clsBrush, New Rectangle(12, 270, 330, 40))
+
+        If bands = 0 Then     'bands is actually = 3
+            formGraphics.FillRectangle(myBrush, New Rectangle(52, 275, 40, 20))
+            formGraphics.FillRectangle(myBrush, New Rectangle(150, 275, 40, 20))
+            formGraphics.FillRectangle(myBrush, New Rectangle(248, 275, 40, 20))
+            formGraphics.DrawLine(Pens.Black, 72, 0, 72, 300)
+            formGraphics.DrawLine(Pens.Black, 170, 0, 170, 300)
+            formGraphics.DrawLine(Pens.Black, 268, 0, 268, 300)
+        End If
+        If bands = 1 Then     'bands is actually = 4
+            formGraphics.FillRectangle(myBrush, New Rectangle(37, 275, 40, 20))
+            formGraphics.FillRectangle(myBrush, New Rectangle(114, 275, 40, 20))
+            formGraphics.FillRectangle(myBrush, New Rectangle(191, 275, 40, 20))
+            formGraphics.FillRectangle(myBrush, New Rectangle(268, 275, 40, 20))
+            formGraphics.DrawLine(Pens.Black, 57, 0, 57, 300)
+            formGraphics.DrawLine(Pens.Black, 134, 0, 134, 300)
+            formGraphics.DrawLine(Pens.Black, 211, 0, 211, 300)
+            formGraphics.DrawLine(Pens.Black, 288, 0, 288, 300)
+        End If
+        If bands = 2 Then     'bands is actually = 5
+            formGraphics.FillRectangle(myBrush, New Rectangle(27, 275, 40, 20))
+            formGraphics.FillRectangle(myBrush, New Rectangle(89, 275, 40, 20))
+            formGraphics.FillRectangle(myBrush, New Rectangle(151, 275, 40, 20))
+            formGraphics.FillRectangle(myBrush, New Rectangle(213, 275, 40, 20))
+            formGraphics.FillRectangle(myBrush, New Rectangle(275, 275, 40, 20))
+            formGraphics.DrawLine(Pens.Black, 47, 0, 47, 300)
+            formGraphics.DrawLine(Pens.Black, 109, 0, 109, 300)
+            formGraphics.DrawLine(Pens.Black, 171, 0, 171, 300)
+            formGraphics.DrawLine(Pens.Black, 233, 0, 233, 300)
+            formGraphics.DrawLine(Pens.Black, 295, 0, 295, 300)
+        End If
+        If bands = 3 Then    'bands is actually = 6
+            formGraphics.FillRectangle(myBrush, New Rectangle(22, 275, 40, 20))
+            formGraphics.FillRectangle(myBrush, New Rectangle(74, 275, 40, 20))
+            formGraphics.FillRectangle(myBrush, New Rectangle(126, 275, 40, 20))
+            formGraphics.FillRectangle(myBrush, New Rectangle(178, 275, 40, 20))
+            formGraphics.FillRectangle(myBrush, New Rectangle(230, 275, 40, 20))
+            formGraphics.FillRectangle(myBrush, New Rectangle(282, 275, 40, 20))
+            formGraphics.DrawLine(Pens.Black, 42, 0, 42, 300)
+            formGraphics.DrawLine(Pens.Black, 94, 0, 94, 300)
+            formGraphics.DrawLine(Pens.Black, 146, 0, 146, 300)
+            formGraphics.DrawLine(Pens.Black, 198, 0, 198, 300)
+            formGraphics.DrawLine(Pens.Black, 250, 0, 250, 300)
+            formGraphics.DrawLine(Pens.Black, 302, 0, 302, 300)
+        End If
+
+        myBrush.Dispose()
+        formGraphics.Dispose()
+
+    End Sub
+
+    Sub makelines()
+        Dim myBrush As New System.Drawing.SolidBrush(System.Drawing.Color.Red)
+        Dim clsBrush As New System.Drawing.SolidBrush(System.Drawing.Color.White)
+        Dim formGraphics As System.Drawing.Graphics
+        Dim pbGraphics As System.Drawing.Graphics
+        formGraphics = Me.CreateGraphics()
+        pbGraphics = Me.PictureBox1.CreateGraphics()
+
+        pbGraphics.DrawLine(Pens.Black, 35, 0, 100, 100)
+        pbGraphics.DrawLine(Pens.Black, 97, 0, 126, 100)
+        pbGraphics.DrawLine(Pens.Black, 159, 0, 149, 100)
+        pbGraphics.DrawLine(Pens.Black, 221, 0, 166, 100)
+        pbGraphics.DrawLine(Pens.Black, 282, 0, 198, 100)
+
+        pbGraphics.DrawLine(Pens.Black, 100, 151, 35, 254)
+        pbGraphics.DrawLine(Pens.Black, 126, 143, 97, 254)
+        pbGraphics.DrawLine(Pens.Black, 149, 143, 159, 254)
+        pbGraphics.DrawLine(Pens.Black, 166, 143, 221, 254)
+        pbGraphics.DrawLine(Pens.Black, 198, 151, 282, 254)
+
+    End Sub
+
+
+    Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles MyBase.Paint
+        Button3_Click(sender, e)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        makelines()
+
+    End Sub
+End Class
